bee8bd845f220d6341ea44a49a695cf001b5b092
ForBai <thelars42@gmail.com>
2023-03-17 22:17:20 +0100

    fixes

--- src/main/kotlin/me/anemoi/sbutlimate/modules/AbilitySwapper.kt
+++ src/main/kotlin/me/anemoi/sbutlimate/modules/AbilitySwapper.kt
@@ -5,14 +5,14 @@ import floppaclient.module.Category
 import floppaclient.module.Module
 import floppaclient.module.SelfRegisterModule
 import floppaclient.module.settings.Visibility
+import floppaclient.module.settings.impl.BooleanSetting
 import floppaclient.module.settings.impl.NumberSetting
 import floppaclient.utils.ChatUtils
-import gg.essential.universal.ChatColor
 import net.minecraft.client.gui.inventory.GuiChest
 import net.minecraft.inventory.ContainerChest
-import net.minecraftforge.client.event.GuiOpenEvent
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraft.item.ItemStack
 import java.util.*
+
 
 @SelfRegisterModule
 object AbilitySwapper : Module(
@@ -27,58 +27,78 @@ object AbilitySwapper : Module(
         1000.0,
         25.0,
         Visibility.VISIBLE,
-        "Delay between interactions in ms"
+        "Delay between interactions in ms, account for ping"
     )
 
     private val startDelay: Double by NumberSetting(
         "Start Delay",
-        25.0,
+        50.0,
         0.0,
         10000.0,
-        100.0,
+        50.0,
         Visibility.VISIBLE,
         "Delay before starting in ms"
     )
 
-    private var stage = 0;
+    private val invertStart: Boolean by BooleanSetting(
+        "Invert Start",
+        false,
+        Visibility.VISIBLE,
+        "Inverts the start"
+    )
 
+    private var slot = false;
+    private var thread: Thread? = null
     override fun onKeyBind() {
         Timer().schedule(object : TimerTask() {
             override fun run() {
                 ChatUtils.sendChat("/hotm")
-                stage = 1
+                if (thread == null || !thread!!.isAlive) {
+                    thread = Thread({
+                        for (i in 0..100) {
+                            val slot29 = mc.thePlayer.openContainer.inventorySlots?.get(29)
+                            val slot33 = mc.thePlayer.openContainer.inventorySlots?.get(33)
+                            if (mc.thePlayer.openContainer is ContainerChest &&
+                                mc.thePlayer.openContainer?.inventorySlots?.get(0)?.inventory?.name?.startsWith("Heart") == true &&
+                                slot29 != null &&
+                                slot33 != null
+                            ) {
+                                if (slot29.stack.isItemEnchanted) {
+                                    mc.playerController.windowClick(
+                                        mc.thePlayer.openContainer.windowId,
+                                        33,
+                                        0,
+                                        0,
+                                        mc.thePlayer
+                                    )
+                                } else if (slot33.stack.isItemEnchanted) {
+                                    mc.playerController.windowClick(
+                                        mc.thePlayer.openContainer.windowId,
+                                        29,
+                                        0,
+                                        0,
+                                        mc.thePlayer
+                                    )
+                                }
+                                Thread.sleep(swapDelay.toLong())
+                                mc.thePlayer.closeScreen()
+                                return@Thread
+                            }
+                            Thread.sleep(20)
+                        }
+                        ChatUtils.modMessage("§aSwapping Ability, timed out")
+                    }, "Ability Swapper")
+                    thread!!.start()
+                }
+
             }
         }, startDelay.toLong())
     }
 
-    @SubscribeEvent
-    fun onGuiTick(event: GuiOpenEvent){
-        val gui = event.gui
-        if (gui !is GuiChest) return
-        val chest = gui as GuiChest
-        val inventory = (chest.inventorySlots as ContainerChest).lowerChestInventory ?: return
 
-        if (ChatColor.stripControlCodes(inventory.displayName.unformattedText)?.lowercase().equals("heart of the mountain")){
-            if (stage == 1){
-                if (inventory.getStackInSlot(29).isItemEnchanted){
-                    Timer().schedule(object : TimerTask() {
-                        override fun run() {
-                            mc.playerController.windowClick(mc.thePlayer.openContainer.windowId, 33, 2, 3, mc.thePlayer)
-                            stage = 0
-                        }
-                    }, swapDelay.toLong())
-                }else if (inventory.getStackInSlot(33).isItemEnchanted){
-                    Timer().schedule(object : TimerTask() {
-                        override fun run() {
-                            mc.playerController.windowClick(mc.thePlayer.openContainer.windowId, 29, 2, 3, mc.thePlayer)
-                            stage = 0
-                        }
-                    }, swapDelay.toLong())
-                }else{
-                    ChatUtils.modMessage("Gui didn't open correctly, please try again")
-                    stage = 0
-                }
-            }
-        }
+    fun getItem(slotNum: Int, currentScreen: GuiChest): ItemStack? {
+        val container = currentScreen.inventorySlots as ContainerChest
+        return container.getSlot(slotNum).stack
     }
+
 }
